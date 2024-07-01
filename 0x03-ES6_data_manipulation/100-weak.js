9a0bf2fc9e761e1b8723628c8254cd49b7cccc50
Emekagold23 <onyekachigold23@gmail.com>
2024-07-01 11:53:37 +0000

    data manipulation

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,16 @@
+/**
+ * A weak map of endpoints and the number of calls made.
+ */
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  let c = weakMap.get(endpoint) || 0;
+
+  c += 1;
+
+  weakMap.set(endpoint, c);
+
+  if (c >= 5) throw Error('Endpoint load is high');
+
+  return c;
+}
